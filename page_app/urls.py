0213bc68ab95705bdd827bc479e4ff22c3944c5d
EduardoakodaP <eduhrepresentacoes1@gmai.com>
2024-03-29 21:18:54 -0300

    ADD alterações

---
+++ page_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from page_app.views import index, contato, services
+
+urlpatterns = [
+    # Cadastrar URLs aqui
+    path('', index),
+
+    path('contato/', contato),
+    path('services/', services)
+]
